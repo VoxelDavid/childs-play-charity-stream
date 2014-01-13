e3e3cdb2a9f4a2215f066ff7e5a85ab61cab1201
David Minnerly <dtreeLive@hotmail.com>
2014-01-13 14:20:41 -0500

    Add humans.txt
    http://humanstxt.org

--- config.rb
+++ config.rb
@@ -31,6 +31,8 @@ end
 #   page "/admin/*"
 # end
 
+page "/humans.txt", :layout => false
+
 # Proxy pages (http://middlemanapp.com/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
