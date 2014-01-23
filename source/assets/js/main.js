8eb690011a673167d17d704db11a154761a06659
David Minnerly <dtreeLive@hotmail.com>
2014-01-22 21:04:38 -0500

    Add configuration for Stellar.js.

--- source/assets/js/main.js
+++ source/assets/js/main.js
@@ -1,7 +1,10 @@
 
-requirejs([
+require([
 	"lib/jquery.min",
 	"lib/jquery.stellar.min",
 ]), function($, stellar) {
-
-}
+	$.stellar({
+		horizontalScrolling: false,
+		responsive: false,
+	});
+};
